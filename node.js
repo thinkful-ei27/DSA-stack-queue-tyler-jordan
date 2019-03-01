69879927901cfa02d64688c0f77f488f0ece97cd
Jordan Heffernan <jheff2@gmail.com>
2019-03-01 08:21:39 -0800

    push da stack

---
+++ node.js
@@ -0,0 +1,8 @@
+class _Node {
+  constructor(data, next) {
+    this.data = data;
+    this.next = next;
+  }
+}
+
+module.exports = _Node;
---
+++ stack.js
@@ -0,0 +1,18 @@
+const _Node = require('./node');
+
+class Stack {
+  constructor() {
+    this.top = null;
+  }
+
+  push(data) {
+    if (this.top === null) {
+      this.top = new _Node(data, null);
+      return this.top;
+    }
+    const node = new _Node(data, this.top);
+    this.top = node;
+  }
+}
+
+module.exports = Stack;
